b37993b372cb2dae717f25010cef95ac48e620ef
k1r4 <jnonstiiven@gmail.com>
2022-12-20 19:04:59 -0500

    moduls

--- python_conprenhension/example_modules/fil.py
+++ python_conprenhension/example_modules/fil.py
@@ -1,3 +1,7 @@
+# %%
+# funcion  toma dos parametros  y filtra un dic 
 def func(data, city):
-    result = list(filter(lambda item: item['city'], city))
+    result = list(filter(lambda item: item['city'] == city, data  ))
     return result
+
+
