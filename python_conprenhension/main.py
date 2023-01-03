f7a4603d844bd649726521ea0fadc183ed57e921
k1r4 <jnonstiiven@gmail.com>
2023-01-03 10:32:35 -0500

    entry_points

---
+++ python_conprenhension/main.py
@@ -0,0 +1,21 @@
+
+# from pkg import mod_1 ,mod_2
+
+
+
+# print(mod_1.func_1())
+# print(mod_1.func_2())
+# print(mod_2.func_3())
+# print(mod_2.func_4())
+
+
+# import pkg.mod_1, pkg.mod_2
+
+
+# #name_sapace , espacio de valor reservado , buena practica 
+# print(pkg.mod_1.func_1())
+suma = lambda x,y: x + y
+
+assert suma(2,3)  ==4
+
+print('hi')
---
+++ python_conprenhension/pkg/__init__.py
@@ -0,0 +1,3 @@
+print('inicio')
+
+URL = 'platzi'
